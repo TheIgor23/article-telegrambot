ceae3a4377104048866d9e9c2441711d32156243
TheIgor23 <egorchik210@gmail.com>
2024-09-29 19:34:48 +0300

    create skeleton for SpringBoot project

---
+++ src/main/java/ru/jezemoin/article_telegrambot/ArticleTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.jezemoin.article_telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jezemoin/article_telegrambot/ArticleTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jezemoin.article_telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
